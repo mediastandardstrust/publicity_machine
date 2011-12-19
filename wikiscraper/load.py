38a625b96cc8f077f4878466a5c7f25feea5615f
Donovan Hide <donny@sur.ly>
2011-12-19 15:38:38 +0000

    use httplib instead so to make use of keep alive

--- wikiscraper/load.py
+++ wikiscraper/load.py
@@ -3,6 +3,7 @@
 
 from StringIO import StringIO
 from lxml import etree
+import httplib
 import urllib
 import urllib2
 import fnmatch
@@ -13,13 +14,15 @@ import bz2
 parser = argparse.ArgumentParser(description='Load wikipedia articles.')
 parser.add_argument('dirname', action='store', help='Path to the parent folder containing bzip files')
 
-SERVER_URL="http://127.0.0.1:8080/document/10/"
+SERVER_URL="127.0.0.1:8080"
+POST_HEADERS={"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
 
-def post(doc_id,data):
-  print "Posting: %s %s %s"%(doc_id,data["id"],data["title"])
-  req=urllib2.Request("%s%s/"%(SERVER_URL,doc_id))
-  req.add_data(urllib.urlencode(data))
-  return urllib2.urlopen(req)
+
+def post(conn,doc_id,data):
+  conn.request("POST","/document/10/%s/"%doc_id,urllib.urlencode(data),POST_HEADERS)
+  response=conn.getresponse()
+  response.read()
+  print "POST: %s %s %s %s"%(response.status,doc_id,data["id"],data["title"])
 
 def process(xml):
   xml="<root>%s</root>"%xml
@@ -42,9 +45,13 @@ def getFiles(path):
       matches.append(os.path.join(root, filename))
   return matches
 
-doc_id=1
-for file in getFiles(parser.parse_args().dirname):
-  for data in process(bz2.BZ2File(file, 'rb').read()):
-    post(doc_id,data)
-    doc_id+=1
+def main():
+  doc_id=1
+  conn = httplib.HTTPConnection(SERVER_URL)
+  for file in getFiles(parser.parse_args().dirname):
+    for data in process(bz2.BZ2File(file, 'rb').read()):
+      post(conn,doc_id,data)
+      doc_id+=1
 
+if __name__ == "__main__":
+  main()
