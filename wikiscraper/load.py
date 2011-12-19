69cf1dbe7a58710ae2a8afbda897749636391bca
Donovan Hide <donny@sur.ly>
2011-12-19 20:31:58 +0000

    multiprocessing version for faster loading

--- wikiscraper/load.py
+++ wikiscraper/load.py
@@ -10,6 +10,7 @@ import fnmatch
 import os
 import argparse
 import bz2
+from multiprocessing import Pool,Lock,Value
 
 parser = argparse.ArgumentParser(description='Load wikipedia articles.')
 parser.add_argument('dirname', action='store', help='Path to the parent folder containing bzip files')
@@ -17,41 +18,42 @@ parser.add_argument('dirname', action='store', help='Path to the parent folder c
 SERVER_URL="127.0.0.1:8080"
 POST_HEADERS={"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
 
-
-def post(conn,doc_id,data):
-  conn.request("POST","/document/10/%s/"%doc_id,urllib.urlencode(data),POST_HEADERS)
+def post(conn,data):
+  with lock:
+    docid.value+=1
+    url="/document/10/%s/"%docid.value
+  conn.request("POST",url,urllib.urlencode(data),POST_HEADERS)
   response=conn.getresponse()
   response.read()
-  print "POST: %s %s %s %s"%(response.status,doc_id,data["id"],data["title"])
+  print "POST: %s %s %s %s"%(response.status,url,data["id"],data["title"])
 
-def process(xml):
-  xml="<root>%s</root>"%xml
+def processFile(filename):
+  conn = httplib.HTTPConnection(SERVER_URL)
+  xml="<root>%s</root>"%bz2.BZ2File(filename, 'rb').read()
   parser=etree.XMLParser(recover=True)
   tree=etree.fromstring(xml,parser=parser)
   etree.strip_tags(tree,"a")
   for doc in tree.xpath("//doc"):
     text=doc.text.encode('utf-8').split("\n")
-    yield {
+    data={
       'id'    : doc.get("id"),
       'title' : text[1],
       'text'  : "\n".join(text[2:]),
       'url'   : doc.get("url").replace("http://it.wikipedia.org","http://en.wikipedia.org")
     }
+    post(conn,data)
 
 def getFiles(path):
   matches = []
   for root, dirnames, filenames in os.walk(path):
     for filename in fnmatch.filter(filenames, '*.bz2'):
-      matches.append(os.path.join(root, filename))
-  return matches
+      yield os.path.join(root,filename)
 
 def main():
-  doc_id=1
-  conn = httplib.HTTPConnection(SERVER_URL)
-  for file in getFiles(parser.parse_args().dirname):
-    for data in process(bz2.BZ2File(file, 'rb').read()):
-      post(conn,doc_id,data)
-      doc_id+=1
+  pool = Pool(processes=4)
+  pool.map(processFile,getFiles(parser.parse_args().dirname))
 
 if __name__ == "__main__":
+  docid = Value('I',1)
+  lock = Lock()
   main()
