94f9079efadf28f61271bdb0776905f1a444ce20
Kaitlin@sfm <klee@sunlightfoundation.com>
2012-03-16 21:48:03 +0000

    finished congressional leadership script

--- congressional_leadership.py
+++ congressional_leadership.py
@@ -11,8 +11,10 @@ from churn.basescraper import BaseScraper
 from dateutil.parser import parse
 from dateutil.tz import * 
 from datetime import *
+import time
 import gzip
 import StringIO
+import feedparser
 
 from util import condense_whitespace
 
@@ -21,12 +23,13 @@ class CongressLeadership(BaseScraper):
             'http://majorityleader.gov/Newsroom/',
             'http://www.democraticleader.gov/news/press',
             'http://www.democraticwhip.gov/newsroom/press-releases',
-            'http://www.majoritywhip.gov/blog',
-            'http://www.reid.senate.gov/newsroom/press_releases.cfm',
-            'http://mcconnell.senate.gov/public/index.cfm?p=PressReleases' ]
+            'http://www.majoritywhip.gov/newsroom',
+            "http://www.reid.senate.gov/newsroom/press_releases.cfm?xml=Senator%20Reid's%20Press%20Releases,RSS2.0full",
+            'http://mcconnell.senate.gov/public/index.cfm?p=PressReleases',
+            'http://www.whitehouse.gov/briefing-room/statements-and-releases' ]
 
-    leaders = ['speaker', 'house_majority_leader', 'house_minority_leader', 'house_minority_whip']
-    sources = ['Speaker of the House Press Releases', 'House Majority Leader Press Releases', 'House Minority Leader Press Releases', 'House Minority Whip Press Releases' ]
+    leaders = ['speaker', 'house_majority_leader', 'house_minority_leader', 'house_minority_whip', 'house_majority_whip', 'senate_majority_leader', 'senate_minority_leader', 'white_house']
+    sources = ['Speaker of the House Press Releases', 'House Majority Leader Press Releases', 'House Minority Leader Press Releases', 'House Minority Whip Press Releases', 'House Majority Whip Press Releases', 'Senate Majority Leader Press Releases', 'Senate Minority Leader Press Releases', 'White House Press Releases' ]
     name = "congressional_leadership_scraper"
     doc_type = 6
     links = []
@@ -47,6 +50,18 @@ class CongressLeadership(BaseScraper):
     def parse_house_minority_whip_date(self, date_text, response, link):
         return parse(self.dates[self.links.index(link)])
 
+    def parse_senate_majority_leader_date(self, date_text, response, link):
+        return parse(self.dates[self.links.index(link)])
+
+    def parse_senate_minority_leader_date(self, date_text, response, link):
+        return parse(self.dates[self.links.index(link)])
+
+    def parse_house_majority_whip_date(self, date_text, response, link):
+        return parse(self.dates[self.links.index(link)])
+
+    def parse_white_house_date(self, date_text, response, link):
+        return parse(self.dates[self.links.index(link)])
+
     def parse_speaker_date(self, date_text, response, link):
         text = html.fromstring(date_text).xpath('//b')
         date = datetime.today()
@@ -58,6 +73,73 @@ class CongressLeadership(BaseScraper):
 
         return date
     
+
+
+
+    def get_speaker_links(self, leader):
+
+        page = html.fromstring(ulib.urlopen(self.urls[self.index]).read())
+        page.make_links_absolute(self.urls[0])
+        link_list = page.find_class('middlelinks')
+        page_count = 1
+        self.links = []
+
+        while link_list and page_count < 3: #don't want to scrape every page every day
+            for item in link_list:
+                link = item.get('href')
+                
+                if link not in self.links:
+                    self.links.append(link)
+
+            page_count += 1 
+            page = html.fromstring(ulib.urlopen(self.urls[0] + '&Page=%s' % page_count).read())
+
+            page.make_links_absolute(self.urls[0])
+            link_list = page.find_class('middlelinks')
+            
+            #need different helper functions for each member of leadership
+            self.extra['leader'] = leader         
+
+    def get_house_majority_leader_links(self, leader):
+
+        page = html.fromstring(ulib.urlopen(self.urls[self.index]).read())
+        page.make_links_absolute(self.urls[1])
+        link_list = page.get_element_by_id('recent_news_2').get_element_by_id('news_text').iterlinks()
+        page_count = 1
+
+        self.links = []
+
+        for l in link_list: #this corresponds to archive pages
+
+            if page_count > 2: break #don't want to get every page every time
+
+            page = html.fromstring(ulib.urlopen(l[2]).read())
+            page.make_links_absolute(l[1])
+            page_links = []
+            page_link_containers = page.find_class('pioneer_inner_3')
+            for p in page_link_containers: page_links.append(p.findall('a'))
+
+            for item in page_links:
+                href = item[0].get('href')     
+                if href not in self.links:
+                    self.links.append(href)
+        
+            page_count += 1 
+
+        #need different helper functions for each member of leadership
+        self.extra['leader'] = leader         
+
+    def get_house_minority_leader_links(self, leader):
+        page = html.fromstring(ulib.urlopen(self.urls[self.index]).read())
+        page.make_links_absolute(self.urls[2].replace('/news/press', ''))
+        self.dates = []
+        self.links = []
+        for item in page.find_class('teaser'):
+            self.links.append(item.find('h3').find('a').get('href'))
+            self.dates.append(item.find_class('date')[0].find('a').text_content())
+
+        self.extra['leader'] = leader
+
     def get_house_minority_whip_links(self, leader):
         #forbidden to bots...
         headers = {
@@ -82,74 +164,58 @@ class CongressLeadership(BaseScraper):
         page.make_links_absolute(self.urls[3].replace('/newsroom/press-releases', ''))
         linklist = page.find_class('views-row')
         self.dates = []
+        self.links = []
         for item in linklist:
-            print item.text_content()
             self.links.append(item.find('.//a').get('href'))
             self.dates.append(item.find_class('date-display-single')[0].text_content())
+            time.sleep(2)
+        self.extra['leader'] = leader
+
+    def get_house_majority_whip_links(self, leader):
+        page = html.fromstring(ulib.urlopen(self.urls[self.index]).read())
+        page.make_links_absolute(self.urls[4].replace('/newsroom', ''))
+        self.dates = []
+        self.links = []
+        for item in page.find_class('list-item'):
+            self.links.append(item.find('h4').find('a').get('href'))
+            self.dates.append(item.find_class('date')[0].text_content())
+
+        self.extra['leader'] = leader
+
+    def get_senate_majority_leader_links(self, leader):
+        
+        feed = feedparser.parse(self.urls[5])
+        self.links = [e.link for e in feed.entries]
+        self.dates = [e.date for e in feed.entries]
+        self.extra['leader'] = leader
+
+    def get_senate_minority_leader_links(self, leader):
+
+        page = html.fromstring(ulib.urlopen(self.urls[self.index]).read())
+        page.make_links_absolute(self.urls[6].replace('/public/index.cfm?p=PressReleases', ''))
+        link_list = page.find_class('recordListTitle')
+        dates = page.find_class('recordListDate')
+        count = 0
+        self.links = []
+        self.dates = []
+        for l in link_list:
+            self.links.append(l.find('a').get('href'))
+            self.dates.append(dates[count].text_content().replace('-', ''))
+            count += 1
+
+        self.extra['leader'] = leader
+
+    def get_white_house_links(self, leader):
+        page = html.fromstring(ulib.urlopen(self.urls[self.index]).read())
+        page.make_links_absolute(self.urls[7].replace('/briefing-room/statements-and-releases',''))
+        link_list = page.find_class('views-row')
+        self.dates = []
+        self.links = []
+        for item in link_list:
+            self.links.append(item.find('.//a').get('href'))
+            self.dates.append(item.find_class('date-line')[0].text_content())
         
         self.extra['leader'] = leader
-
-    def get_house_minority_leader_links(self, leader):
-        page = html.fromstring(ulib.urlopen(self.urls[self.index]).read())
-        page.make_links_absolute(self.urls[2].replace('/news/press', ''))
-        self.dates = []
-        for item in page.find_class('teaser'):
-            self.links.append(item.find('h3').find('a').get('href'))
-            self.dates.append(item.find_class('date')[0].find('a').text_content())
-
-        self.extra['leader'] = leader
-
-    def get_house_majority_leader_links(self, leader):
-
-        page = html.fromstring(ulib.urlopen(self.urls[self.index]).read())
-        page.make_links_absolute(self.urls[0])
-        link_list = page.get_element_by_id('recent_news_2').get_element_by_id('news_text').iterlinks()
-        page_count = 1
-
-        for l in link_list: #this corresponds to archive pages
-            print "Parent page: %s" % l[2]
-            page = html.fromstring(ulib.urlopen(l[2]).read())
-            page.make_links_absolute(l[2])
-            page_links = []
-            page_link_containers = page.find_class('pioneer_inner_3')
-            for p in page_link_containers: page_links.append(p.findall('a'))
-
-            print len(page_links)
-            for item in page_links:
-                href = item[0].get('href')     
-                if href not in self.links:
-                    self.links.append(href)
-                    print "got %s from page %s" % (href, page_count)
-
-            page_count += 1 
-
-        #need different helper functions for each member of leadership
-        self.extra['leader'] = leader         
-
-
-    def get_speaker_links(self, leader):
-
-        page = html.fromstring(ulib.urlopen(self.urls[self.index]).read())
-        page.make_links_absolute(self.urls[0])
-        link_list = page.find_class('middlelinks')
-        page_count = 1
-
-        while link_list: # and page_count < 3:
-            for item in link_list:
-                link = item.get('href')
-                
-                if link not in self.links:
-                    self.links.append(link)
-                    print "got %s from page %s" % (link, page_count)
-
-            page_count += 1 
-            page = html.fromstring(ulib.urlopen(self.urls[0] + '&Page=%s' % page_count).read())
-
-            page.make_links_absolute(self.urls[0])
-            link_list = page.find_class('middlelinks')
-            
-            #need different helper functions for each member of leadership
-            self.extra['leader'] = leader         
 
 
     def extract(self, response, link):
@@ -162,7 +228,6 @@ class CongressLeadership(BaseScraper):
         #how to find out where speaker links start?
         date = getattr(self, 'parse_%s_date'% self.extra['leader'])(body, response, link) 
 
-        print date
         #strip extra html readability leaves in, like p tags
         title = html.fromstring(title).text_content()
         body = html.fromstring(body).text_content()
@@ -190,15 +255,23 @@ class CongressLeadership(BaseScraper):
             self.get_house_minority_leader_links(self.leaders[self.index])
         elif self.index == 3:
             self.get_house_minority_whip_links(self.leaders[self.index])
-        print self.links
+        elif self.index == 4:
+            self.get_house_majority_whip_links(self.leaders[self.index])
+        elif self.index == 5:
+            self.get_senate_majority_leader_links(self.leaders[self.index])
+        elif self.index == 6:
+            self.get_senate_minority_leader_links(self.leaders[self.index])
+        elif self.index == 7:
+            self.get_white_house_links(self.leaders[self.index])
+
         self.process_batch(self.links)
 
 
 if __name__ == '__main__':
-#   scraper = CongressLeadership(0)
-#   scraper = CongressLeadership(1)
-#    scraper = CongressLeadership(2)
-    scraper = CongressLeadership(3)
-    scraper.main()
+#    scraper = CongressLeadership(7)
+#    scraper.main()
+    for index in [0,1,2,3,4,5,6,7]:
+        scraper = CongressLeadership(index)
+        scraper.main()
 
 
