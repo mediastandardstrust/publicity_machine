bc6beef571ac816feb302e2909d5c93653f4e33f
Drew Vogel <dvogel@sunlightfoundation.com>
2012-03-29 13:36:50 -0400

    Rearranged the parallelization to avoid 2.6/2.7 pool.join() difference.

--- wikiscraper/load.py
+++ wikiscraper/load.py
@@ -13,6 +13,7 @@ from lxml import etree
 from multiprocessing import Pool, cpu_count
 
 parser = argparse.ArgumentParser(description='Load wikipedia articles.')
+parser.add_argument('--verbose', '-v', action='count', help='Print confirmation for each document submitted to the SuperFastMatch server.')
 parser.add_argument('dirname', action='store', help='Path to the parent folder containing bzip files')
 parser.add_argument('host', action='store', help='Hostname of the SuperFastMatch server.')
 parser.add_argument('port', action='store', help='Port of the SuperFastMatch server.')
@@ -24,12 +25,21 @@ def add_document(data, docid):
                                                                 port=args.port), 
                               parse_response=True)
   sfm.add(args.doctype, docid, **data)
-  print "{0}, {1}: '{2}'".format(args.doctype, docid, data['title'])
+  if args.verbose:
+    print "{0}, {1}: '{2}'".format(args.doctype, docid, data['title'])
 
-def processFile(pool, first_docid, filename):
+def create_pool():
+  processes = 2
+  try:
+    processes = int(math.ceil(cpu_count() * 2))
+  except NotImplementedError:
+    pass
+  return Pool(processes=processes)
+
+def processFile(first_docid, filename):
+  pool = create_pool()
   with file(filename, 'rb') as xmlfile:
     xml = "<root>%s</root>" % xmlfile.read()
-  pool.join()
   parser=etree.XMLParser(recover=True)
   tree=etree.fromstring(xml,parser=parser)
   etree.strip_tags(tree,"a")
@@ -46,6 +56,8 @@ def processFile(pool, first_docid, filename):
       'url'   : doc.get("url", "").replace("http://it.wikipedia.org","http://en.wikipedia.org")
     }
     pool.apply_async(add_document, [attrs, docid])
+  pool.close()
+  pool.join()
   print "Processed %s docs from %s" % (len(docs), filename)
   return docs[-1][0]
 
@@ -54,21 +66,14 @@ def getFiles(path):
     for filename in fnmatch.filter(filenames, 'wiki*'):
       yield os.path.join(root,filename)
 
-def main(pool):
+def main():
   previous_docid = 1
-  for filename in getFiles(args.dirname):
-    print "Processing %s" % filename
-    previous_docid = processFile(pool, previous_docid + 1, filename)
+  files = list(getFiles(args.dirname))
+  files.sort()
+  for (filenum, filename) in enumerate(files, start=1):
+    print "Processing %s (%s of %s)" % (filename, filenum, len(files))
+    previous_docid = processFile(previous_docid + 1, filename)
   print "Processed all files, waiting for workers to terminate."
-  pool.close()
-  pool.join()
 
 if __name__ == "__main__":
-  processes = 2
-  try:
-    processes = int(math.ceil(cpu_count() / 2))
-  except NotImplementedError:
-    pass
-  
-  pool = Pool(processes=processes)
-  main(pool)
+  main()
