64bd83056ca3856c1bfac8dfd5e6733759569da0
Drew Vogel <dvogel@sunlightfoundation.com>
2012-03-30 15:29:08 -0400

    Dropped parallelism because multiprocessing was causing a soft lockup on EC2. Added --mindoclen filtering option.

--- wikiscraper/load.py
+++ wikiscraper/load.py
@@ -10,10 +10,10 @@ import argparse
 import superfastmatch
 from pprint import pprint
 from lxml import etree
-from multiprocessing import Pool, cpu_count
 
 parser = argparse.ArgumentParser(description='Load wikipedia articles.')
 parser.add_argument('--verbose', '-v', action='count', help='Print confirmation for each document submitted to the SuperFastMatch server.')
+parser.add_argument('--mindoclen', action='store', type=int, default=360, metavar='LEN', help='The minimum character length of a document in order to be included, defaults to 360 characters or approximately 4 sentences.')
 parser.add_argument('dirname', action='store', help='Path to the parent folder containing bzip files')
 parser.add_argument('host', action='store', help='Hostname of the SuperFastMatch server.')
 parser.add_argument('port', action='store', help='Port of the SuperFastMatch server.')
@@ -34,30 +34,23 @@ def add_document(data, docid):
   sfm = superfastmatch.Client(url='http://{host}:{port}'.format(host=args.host, 
                                                                 port=args.port), 
                               parse_response=True)
-  result = sfm.add(args.doctype, docid, **data)
+  result = sfm.add(args.doctype, docid, defer=True, **data)
   if result['success'] == True:
     if args.verbose:
       print "{0}, {1}: '{2}'".format(args.doctype, docid, data['title'])
   else:
       print "Failed to add '{0}'".format(data['title'])
 
-def create_pool():
-  processes = 2
-  try:
-    processes = int(math.ceil(cpu_count() / 2))
-  except NotImplementedError:
-    pass
-  return Pool(processes=processes)
-
 def processFile(first_docid, filename):
-  pool = create_pool()
   with file(filename, 'rb') as xmlfile:
     xml = "<root>%s</root>" % xmlfile.read()
   parser=etree.XMLParser(recover=True)
   tree=etree.fromstring(xml,parser=parser)
   etree.strip_tags(tree,"a")
   def inclusion_filter(doc):
-    return (not doc.get('url', '').endswith(u'_(disambiguation)') and not doc.get('url', '').startswith(u'List_of'))
+    return (not doc.get('url', '').endswith(u'_(disambiguation)') 
+            and not doc.get('url', '').startswith(u'List_of')
+            and len(doc.text.strip() >= args.mindoclen))
   (kept, dropped) = bifurcate(inclusion_filter, tree.xpath('//doc'))
   print "Dropped %s of %s documents" % (len(dropped), len(kept) + len(dropped))
   docs = list(enumerate(kept, start=first_docid))
@@ -69,9 +62,7 @@ def processFile(first_docid, filename):
       'text'  : "\n".join(text[2:]),
       'url'   : doc.get("url", "").replace("http://it.wikipedia.org","http://en.wikipedia.org")
     }
-    pool.apply_async(add_document, [attrs, docid])
-  pool.close()
-  pool.join()
+    add_document(attrs, docid)
   print "Processed %s docs from %s" % (len(docs), filename)
   return docs[-1][0]
 
